48717253a90bd25183492d62ad800edd892b3bc9
Ajay <ajay.143444@gmail.com>
2020-11-25 01:09:42 +0530

    added test case for getCountries

--- src/test/java/com/covid/tracker/CovidTrackerApplicationTests.java
+++ src/test/java/com/covid/tracker/CovidTrackerApplicationTests.java
@@ -1,7 +1,14 @@
 package com.covid.tracker;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.covid.tracker.model.Country;
+import com.covid.tracker.service.CovidDetailsServiceImpl;
 
 @SpringBootTest
 class CovidTrackerApplicationTests {
@@ -10,4 +17,12 @@ class CovidTrackerApplicationTests {
 	void contextLoads() {
 	}
 
+	
+	
+	@Test
+	void testgetCountries() {
+		CovidDetailsServiceImpl covidDetailsServiceImpl = new CovidDetailsServiceImpl();
+		List<Country> listCont = covidDetailsServiceImpl.getCountries(true);
+		assertTrue(!listCont.isEmpty());
+	}
 }
